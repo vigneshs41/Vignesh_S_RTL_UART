5e619c265376d851dfeb451417559a8c0d568e14
vigneshs41 <vigneshsampath742000@gmail.com>
2025-09-15 00:04:08 +0530

    modifed uart tx

--- rtl/uart_tx.v
+++ rtl/uart_tx.v
@@ -93,8 +93,8 @@ always @(posedge clk) begin
             if (s_axis_tvalid) begin
                 s_axis_tready_reg <= !s_axis_tready_reg;
                 prescale_reg <= (prescale << 3)-1;
-                bit_cnt <= DATA_WIDTH+1;
-                data_reg <= {1'b1, s_axis_tdata};
+                bit_cnt <= DATA_WIDTH;
+                data_reg <= s_axis_tdata;
                 txd_reg <= 0;
                 busy_reg <= 1;
             end
@@ -102,10 +102,11 @@ always @(posedge clk) begin
             if (bit_cnt > 1) begin
                 bit_cnt <= bit_cnt - 1;
                 prescale_reg <= (prescale << 3)-1;
-                {data_reg, txd_reg} <= {1'b0, data_reg};
+                
+                {txd_reg,data_reg} <= {1'b0, data_reg};
             end else if (bit_cnt == 1) begin
                 bit_cnt <= bit_cnt - 1;
-                prescale_reg <= (prescale << 3);
+                prescale_reg <= (prescale << 3) -1;
                 txd_reg <= 1;
             end
         end
